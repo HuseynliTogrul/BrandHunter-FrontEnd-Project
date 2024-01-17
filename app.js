618a78b296bb1d526e1b730875b866e45b440c94
togrul <toghrulih@code.local>
2024-01-17 17:12:25 +0400

    BrandHunter project

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const catalogTypeTop = document.querySelectorAll(".catalogTypeTop")
 const catalogtypeDrop = document.querySelectorAll(".catalogtype-drop")
 const chevronIcon = document.querySelectorAll(".chevronIcon")
 
+const itemSort = document.querySelector(".item-sort")
+const sortList = document.querySelector(".sort-list ul")
+
+const itemHeader = document.querySelectorAll(".item-header")
+const itemHeaderIcon = document.querySelector(".item-header img")
+const itemList = document.querySelectorAll(".item-list")
+
+
 navbarIcon.addEventListener("click", () => {
     catalogDropdown.classList.toggle("hidden");
 })
@@ -16,4 +24,29 @@ catalogTypeTop.forEach((catalog, index) => {
         catalogtypeDrop[index].classList.toggle("hidden");
         chevronIcon[index].classList.toggle("rotate");
     });
+});
+
+
+window.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+        catalogDropdown.classList.add("hidden");
+    }
+})
+
+itemSort.addEventListener("click", () => {
+    sortList.classList.toggle("hidden");
+});
+
+itemHeader.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        itemList[index].classList.toggle("hidden")
+        if (itemList[index].classList == "item-list hidden") {
+            itemHeaderIcon.src = `../assets/icons/plus.svg`;
+
+            console.log(1);
+        } else if(itemList[index].classList == "item-list") {
+            itemHeaderIcon.src = `../assets/icons/minus.svg`;
+            console.log(2);
+        }
+    })
 });
